750ee021c7c352502abcd0d1b97b824d45caf153
thiagoshibata <thiago.shibatta@gmail.com>
2025-01-20 08:46:37 -0300

    feat: Implementing select actions

--- src/models/repository/orders_repository.py
+++ src/models/repository/orders_repository.py
@@ -1,3 +1,5 @@
+from bson.objectid import ObjectId
+
 class OrdersRepository:
     def __init__(self, db_connection) -> None:
         self.__collection_name = "orders"
@@ -10,3 +12,37 @@ class OrdersRepository:
     def insert_list_of_documents(self, list_of_documents: list) -> None:
         collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_many(list_of_documents)
+
+    def select_many(self, doc_filter: dict) -> list:
+        """ select multiple documents according to the filter """
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(doc_filter)
+        return data
+
+    def select_one(self, doc_filter: dict) -> dict:
+        """ select the first element found """
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        response = collection.find_one(doc_filter)
+        return response
+
+    def select_many_with_properties(self, doc_filter: dict) -> list:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(
+            doc_filter, # search filter
+            { "_id": 0, "cupom": 0 } # Return options | removing properties of the return
+        )
+        return data
+
+    def select_if_property_exists(self) -> dict:
+        """select elements if properties exists"""
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        response = collection.find(
+            {"address": {"$exists": True}}, # Only exists field
+            { "_id": 0, "cupom":0 } # removing properties of the return
+        )
+        return response
+
+    def select_by_object_id(self, object_id: str) -> dict:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find_one({"_id" : ObjectId(object_id)})
+        return data
--- src/models/repository/orders_repository_test.py
+++ src/models/repository/orders_repository_test.py
@@ -17,3 +17,77 @@ def test_insert_list_of_documents():
     orders_repository = OrdersRepository(conn)
     my_docs = [{ "elem1": "coisa1"}, {"elem2":"coisa2"}, {"elem3":"coisa3"}]
     orders_repository.insert_list_of_documents(my_docs)
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_select_many():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = { "cupom" : False }
+    response = orders_repository.select_many(doc_filter)
+    print()
+    for doc in response:
+        print(doc)
+        print(doc["itens"])
+        print()
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_select_one():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = { "client" : "Thiago" }
+    response = orders_repository.select_one(doc_filter)
+    print()
+    print(response)
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_select_many_with_properties():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = { "cupom" : True }
+    response = orders_repository.select_many_with_properties(doc_filter)
+    print()
+    for doc in response:
+        print(doc)
+        print()
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_select_if_property_exists():
+    orders_repository = OrdersRepository(conn)
+    response = orders_repository.select_if_property_exists()
+    print()
+    for doc in response:
+        print(doc)
+        print()
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_select_many_with_multiple_filters():
+    """Similar to the “AND” search in SQL"""
+    orders_repository = OrdersRepository(conn)
+    doc_filter = { "cupom" : True,
+                  "itens.doce": {"$exists": True}
+                  }
+    response = orders_repository.select_many(doc_filter)
+    print()
+    for doc in response:
+        print(doc)
+        print()
+
+@pytest.mark.skip(reason="interacao com o banco de dados")
+def test_select_many_with_or_filter():
+    """Similar to the "OR" search in SQL"""
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {
+        "$or":[
+            {"address":{"$exists": True}},
+            {"itens.doce.tipo": "chocolate"}
+        ]
+    }
+    response = orders_repository.select_many(doc_filter)
+    print()
+    for doc in response:
+        print(doc)
+        print()
+
+def test_select_by_object_id():
+    orders_repository = OrdersRepository(conn)
+    object_id = "6784fb2afac3010aef8f106f"
+    response = orders_repository.select_by_object_id(object_id)
+    print()
+    print(response)
