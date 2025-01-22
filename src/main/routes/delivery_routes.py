2de75c6c74df906e674393d9ad31b6653f03e1d1
thiagoshibata <thiago.shibatta@gmail.com>
2025-01-22 11:07:09 -0300

    feat: Adding use case in registry order route

--- src/main/routes/delivery_routes.py
+++ src/main/routes/delivery_routes.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, jsonify, request
 from src.main.http_types.http_request import HttpRequest
+from src.main.composer.registry_order_composer import registry_order_composer
 
 delivery_routes_bp = Blueprint("delivery_routes", __name__)
 
 @delivery_routes_bp.route("/delivery/order", methods=["POST"])
 def registry_order():
+    use_case = registry_order_composer()
     http_request = HttpRequest(body=request.json)
     # Will send HttpResponse to logic
+    response = use_case.registry(http_request)
     # The logic will return an HttpResponse
-    return jsonify({"Ola": "Mundo"}), 200
+
+    return jsonify(response.body), response.status_code
